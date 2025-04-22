2545de55e541e41e4deeded3349d24f215061c6b
Jeroen Gerits <jeroen@gerits.email>
2025-04-22 15:43:29 +0200

    wip

--- src/Twine.php
+++ src/Twine.php
@@ -4,15 +4,22 @@ declare(strict_types=1);
 
 namespace JeroenGerits\Twine;
 
+use Arr;
+
 readonly class Twine
 {
     public function __construct(
-        protected array $classesInput
+        protected array $inputArr
     ) {}
 
     public static function make(mixed ...$input)
     {
         return new self($input);
+    }
+
+    public function toArray(): array
+    {
+        return Arr::flatten($this->inputArr);
     }
 
     public function toString(): string
